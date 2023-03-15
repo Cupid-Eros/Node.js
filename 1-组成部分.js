99ba6001847d59d6a5b9e9000ee637d58564c2ae
连营 魏 <W18834184406@outlook.com>
2023-03-15 18:38:00 +0800

    node

--- 1-组成部分.js
+++ 1-组成部分.js
@@ -5,6 +5,9 @@ var http=require("http");
 
 // http.createServer() 方法创建服务器，并使用 listen 方法绑定 8888 端口
 http.createServer(function (request,response) {
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
     response.writeHead(200,{'Content-Type':'text/plain'});
 
     response.end('Hello world\n');
