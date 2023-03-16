ad487c37d51018e91e85f86fec945ffda5fca561
连营 魏 <W18834184406@outlook.com>
2023-03-16 18:11:15 +0800

    node

---
+++ 2-使用 nvm 管理不同版本的 node 与 npm.js
@@ -0,0 +1,19 @@
+//npm install npm -g
+//npm -v
+//nvm(Node Version Manager) use 4.2.2
+
+
+//npm install <Module Name>
+//==>npm install express
+//npm install express          # 本地安装
+//npm install express -g   # 全局安装
+//var express = require('express');
+
+// 本地安装
+// 1. 将安装包放在 ./node_modules 下（运行 npm 命令时所在的目录），如果没有 node_modules 目录，会在当前执行 npm 命令的目录下生成 node_modules 目录。
+// 2. 可以通过 require() 来引入本地安装的包。
+// 全局安装
+// 1. 将安装包放在 /usr/local 下或者你 node 的安装目录。
+// 2. 可以直接在命令行里使用。
+
+//npm list -g
