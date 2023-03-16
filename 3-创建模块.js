6412f981fd7f0456c490a49b8ad98392aae611d7
连营 魏 <W18834184406@outlook.com>
2023-03-16 18:28:06 +0800

    node

---
+++ 3-创建模块.js
@@ -0,0 +1,45 @@
+// $ npm init
+// This utility will walk you through creating a package.json file.
+// It only covers the most common items, and tries to guess sensible defaults.
+
+// See `npm help json` for definitive documentation on these fields
+// and exactly what they do.
+
+// Use `npm install <pkg> --save` afterwards to install a package and
+// save it as a dependency in the package.json file.
+
+// Press ^C at any time to quit.
+// name: (node_modules) runoob                   # 模块名
+// version: (1.0.0) 
+// description: Node.js 测试模块(www.runoob.com)  # 描述
+// entry point: (index.js) 
+// test command: make test
+// git repository: https://github.com/runoob/runoob.git  # Github 地址
+// keywords: 
+// author: 
+// license: (ISC) 
+// About to write to ……/node_modules/package.json:      # 生成地址
+
+// {
+//   "name": "runoob",
+//   "version": "1.0.0",
+//   "description": "Node.js 测试模块(www.runoob.com)",
+//   ……
+// }
+
+
+// Is this ok? (yes) yes
+
+
+// 以上的信息，你需要根据你自己的情况输入。在最后输入 "yes" 后会生成 package.json 文件。
+
+// 接下来我们可以使用以下命令在 npm 资源库中注册用户（使用邮箱注册）：
+
+// $ npm adduser
+// Username: mcmohd
+// Password:
+// Email: (this IS public) mcmohd@gmail.com
+// 接下来我们就用以下命令来发布模块：
+
+// $ npm publish
+// 如果你以上的步骤都操作正确，你就可以跟其他模块一样使用 npm 来安装。
